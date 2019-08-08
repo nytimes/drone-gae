5030635ddb5d768925b6cd256e364e4ef3e2e39c
Madireddy <al.madireddy@gmail.com>
2019-08-08 14:12:43 -0400

    unit test for redactions

--- exec_test.go
+++ exec_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -23,4 +25,11 @@ func TestEnvironRun(t *testing.T) {
 		assert.Equal(t, "hello, ae\n", stdout.String())
 		assert.Equal(t, "", stderr.String())
 	}
+}
+
+func TestRedaction(t *testing.T) {
+	arg := "--oauth2_access_token hello -E CREDENTIALS:{\n \"name\": \"nameVal\", \n \"name\": \n \"nameVal\", \n \"name\": \"nameVal\"\n }"
+	redacted := reRedact.ReplaceAllString(strings.Trim(fmt.Sprint(arg), "[]"), " $1 [redacted]")
+
+	assert.Equal(t, " --oauth2_access_token  [redacted] -E CREDENTIALS: [redacted]", redacted)
 }
